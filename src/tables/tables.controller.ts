7284de5c8f5eaaed8a956e9ac68e4125fadbdb4d
getill <theo.lemray@gmail.com>
2025-11-28 10:45:14 +0100

    Tables added and visible via GET

--- src/tables/tables.controller.ts
+++ src/tables/tables.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get , Param, NotFoundException} from '@nestjs/common';
+import { Table, TablesService } from './tables.service';
 
 @Controller('tables')
-export class TablesController {}
+export class TablesController {
+    constructor(private readonly tablesService: TablesService) {}
+
+    @Get()
+    findAll(): Table[] {
+        return this.tablesService.findAll();
+    }
+
+    @Get(':name')
+    findOne(@Param('name') name: string): Table[] {
+        const table = this.tablesService.findOne(name);
+        if (!table) {
+            throw new NotFoundException(`La table ${name} n'existe pas`);
+        }
+        return table;
+    }
+}
--- src/tables/tables.service.ts
+++ src/tables/tables.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
 
+export type Table = any
+
 @Injectable()
-export class TablesService {}
+export class TablesService {
+    private readonly tables = [
+        {
+            tableId: 1,
+            tablename: 'Noobs',
+            smallblind: 10,
+            bigblind: 20,
+        },
+        {
+            tableId: 2,
+            tablename: 'Rookies',
+            smallblind: 20,
+            bigblind: 40,
+        },
+        {
+            tableId: 3,
+            tablename: 'Masters',
+            smallblind: 100,
+            bigblind: 200,
+        },
+
+    ];
+
+    findAll(): Table {
+        return this.tables;
+    }
+    findOne(tablename: string): Table | undefined {
+        return this.tables.find((table) => table.tablename === tablename);
+    }
+}
